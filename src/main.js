4c16a6f1a73450a84c6c58a2ab7011cbf1f3c208
Axel Martínez <amartinez@goliath.local>
2017-11-03 11:53:03 -0600

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import {
+  Vuetify,
+  VApp,
+  VNavigationDrawer,
+  VFooter,
+  VList,
+  VBtn,
+  VIcon,
+  VGrid,
+  VToolbar,
+  transitions
+} from 'vuetify'
+import App from './App.vue'
+
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VNavigationDrawer,
+    VFooter,
+    VList,
+    VBtn,
+    VIcon,
+    VGrid,
+    VToolbar,
+    transitions
+  }
+})
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
